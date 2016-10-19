04442ccb88d39e840235747cac2aaec5bcd329de
ison.dell <ison@dell.com>
2016-10-20 00:54:18 +0800

    mm

--- test_springload_bad_beans/src/main/java/z/server/AppStart.java
+++ test_springload_bad_beans/src/main/java/z/server/AppStart.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
 
-@SpringBootApplication(scanBasePackages = "cn.ptp.*")
+@SpringBootApplication(scanBasePackages = "cn.ptp.demo.*")
 public class AppStart {
 
 	public static void main(String[] args)
