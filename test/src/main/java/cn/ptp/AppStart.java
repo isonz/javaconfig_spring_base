f0a48f1bd715d8d09056467a444cedc5174ade02
Ison <ison.chang@gmail.com>
2016-10-20 14:46:32 +0800

    Zcsc

--- test/src/main/java/cn/ptp/AppStart.java
+++ test/src/main/java/cn/ptp/AppStart.java
@@ -2,21 +2,12 @@ package cn.ptp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"cn.ptp"})
 public class AppStart {
 
 	public static void main(String[] args)
 	{
-		ApplicationContext ctx = SpringApplication.run(AppStart.class, args);
-		/*
-		System.out.println("Spring 注入的 Beans :");
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
-		*/
+		SpringApplication.run(AppStart.class, args);
 	}
 }
