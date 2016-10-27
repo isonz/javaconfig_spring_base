783f0de037e754a88ec5a31a735a4e615a794710
Ison <ison.chang@gmail.com>
2016-10-27 11:00:43 +0800

    adsfsdf

--- init/src/main/java/cn/ptp/AppStart.java
+++ init/src/main/java/cn/ptp/AppStart.java
@@ -4,19 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class AppStart {
 
 	public static void main(String[] args)
 	{
 		ApplicationContext ctx = SpringApplication.run(AppStart.class, args);
+		/*
 		System.out.println("Spring 注入的 Beans :");
 		String[] beanNames = ctx.getBeanDefinitionNames();
 		Arrays.sort(beanNames);
 		for (String beanName : beanNames) {
 			System.out.println(beanName);
 		}
+		*/
 	}
 }
